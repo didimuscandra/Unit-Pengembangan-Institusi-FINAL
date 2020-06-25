5f741d17ca18d4decdb367efadb4486aaf33f38b
didimuscandra <didimuscandra@gmail.com>
2020-06-25 22:51:59 +0700

    kegiatan fix

--- resources/views/Kegiatan/create.blade.php
+++ resources/views/Kegiatan/create.blade.php
@@ -38,8 +38,8 @@
           </div>
         </div>
         <div class="form-group">
-          <label for="rencana_donasi" class="col-md-12">Rencana Donasi</label>
-          <div class="col-md-2">
+          <label for="namaKegiatan" class="col-md-12">Rencana Donasi</label>
+          <div class="col-md-3">
             <input type="number" class="form-control" id="rencana_donasi" placeholder="" name="rencana_donasi" class="form-control form-control-line" required> 
           </div>
         </div>
@@ -53,33 +53,6 @@
 </div>
 @endsection()
 
-@section('footer')
-<script type="text/javascript">
-var rupiah = document.getElementById("rencana_donasi");
-rupiah.addEventListener("keyup", function(e) {
-  // tambahkan 'Rp.' pada saat form di ketik
-  // gunakan fungsi formatRupiah() untuk mengubah angka yang di ketik menjadi format angka
-  rupiah.value = formatRupiah(this.value, "Rp. ");
-});
 
-/* Fungsi formatRupiah */
-function formatRupiah(angka, prefix) {
-  var number_string = angka.replace(/[^,\d]/g, "").toString(),
-    split = number_string.split(","),
-    sisa = split[0].length % 3,
-    rupiah = split[0].substr(0, sisa),
-    ribuan = split[0].substr(sisa).match(/\d{3}/gi);
-
-  // tambahkan titik jika yang di input sudah menjadi angka ribuan
-  if (ribuan) {
-    separator = sisa ? "." : "";
-    rupiah += separator + ribuan.join(".");
-  }
-
-  rupiah = split[1] != undefined ? rupiah + "," + split[1] : rupiah;
-  return prefix == undefined ? rupiah : rupiah ? "Rp. " + rupiah : "";
-}
-</script>
-@endsection()
 
         
